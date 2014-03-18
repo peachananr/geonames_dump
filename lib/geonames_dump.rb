d89454485c6d22476e7de99a8bde5f15d55f4edc
peachananr <peachananr@gmail.com>
2014-03-18 15:06:34 +0700

    Add continent search 7

--- lib/geonames_dump.rb
+++ lib/geonames_dump.rb
@@ -13,9 +13,9 @@ module GeonamesDump
         continent = GeonamesCountry.where("continent_s ILIKE '%#{query}%'").select("continent_s as name, 'continent' as tag_type").group("continent_s").limit(options[:limit])
         countries = GeonamesCountry.where("country ILIKE '%#{query}%'").select("country, continent_s, 'country' as tag_type").limit(options[:limit])
         
-        states = GeonamesAdmin1.joins("left join geonames_countries on country_code = iso").where("name ILIKE '%#{query}%' or asciiname ILIKE '%#{query}%' or alternatenames ILIKE '%#{query}%'").select("geonames_features.*,geonames_countries.country as country, 'states' as tag_type").limit(options[:limit])
+        states = GeonamesAdmin1.joins("left join geonames_countries on country_code = iso").where("name ILIKE '%#{query}%' or asciiname ILIKE '%#{query}%' or alternatenames ILIKE '%#{query}%'").select("geonames_features.*,geonames_countries.country as country,geonames_countries.continent_s as continent_s, 'states' as tag_type").limit(options[:limit])
         
-        cities = GeonamesCity.joins("left join geonames_countries on country_code = iso").where("search_vector @@ plainto_tsquery('#{query}')").select("geonames_features.*,geonames_countries.country as country, 'city' as tag_type")
+        cities = GeonamesCity.joins("left join geonames_countries on country_code = iso").where("search_vector @@ plainto_tsquery('#{query}')").select("geonames_features.*,geonames_countries.country as country,geonames_countries.continent_s as continent_s, 'city' as tag_type")
         
         ret = continent + countries + states + cities 
       else # country, or specific type
